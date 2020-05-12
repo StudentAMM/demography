a54fd5638c2326dd13c6e50f0e688dcedf85c1de
Коротких А.А <evolenteam@mail.ru>
2020-05-13 00:53:36 +0300

    The calculation of the ratio of male and female population and population

--- src/main/scala/solution/Job.scala
+++ src/main/scala/solution/Job.scala
@@ -5,6 +5,9 @@ import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.{SparkConf, SparkContext}
 import solution.domain.{CityPopulation, _}
 
+
+case class Result(uniqSex: Set[String], value: Seq[Double], sumPopulation: Double)
+
 object Job extends App {
 
   val sparkConf = new SparkConf()
@@ -17,4 +20,30 @@ object Job extends App {
   val population = sc.textFile(Parameters.path_population)
     .map(CityPopulation(_))
 
+  // Необходимо реализовать job, рассчитывающий:
+  //  население стран,
+  //  для каждой страны:
+  //    o  количество городов-миллионников,
+  //    o  топ 5 самых крупных городов,
+  //    o  соотношение мужского и женского населения.
+
+  population.map(pp => ((pp.countryOrArea, pp.year, pp.city, pp.cityType, pp.recordType, pp.sourceYear), (pp.sex, pp.value)))
+    .aggregateByKey(Result(Set.empty[String], Seq.empty[Double], 0))((acc, pp) => {
+      val uniq = acc.uniqSex + pp._1
+      val uniqNum = acc.value :+ pp._2
+      Result(uniq, uniqNum, acc.sumPopulation+pp._2)
+    }, (one, two) => {
+      if (two.uniqSex.last == "Male")
+        Result(one.uniqSex,
+          one.value ++ two.value,
+          one.sumPopulation + two.sumPopulation)
+      else
+        Result(one.uniqSex,
+          two.value ++ one.value,
+          one.sumPopulation + two.sumPopulation)
+    }).map{
+    case (key, result) =>     // head - male    last - female  result - both
+      //s"$key\t${result.value.size}\t${result.value.head\t${result.value.last}\t${result.sumPopulation}"
+      s"$key\t${result.value.size}\t${result.value.head/result.value.last}\t${result.sumPopulation}"
+  }.saveAsTextFile(Parameters.output_path)
 }
