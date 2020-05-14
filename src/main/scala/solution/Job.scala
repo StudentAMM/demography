8048cec83a1f1cec4d154149f521a9bb4c930029
Коротких А.А <evolenteam@mail.ru>
2020-05-14 22:59:46 +0300

    Add search for focal values

--- src/main/scala/solution/Job.scala
+++ src/main/scala/solution/Job.scala
@@ -14,9 +14,9 @@ import solution.domain.{CityPopulation, _}
 
 case class Result(uniqSex: Set[String], value: Seq[Double], sumPopulation: Double)
 
+case class Result2(countCity: Int, city: Seq[(String, Double)])
 
-case class Result2(uniqSex: Seq[String], value: Seq[Double], reliability: Seq[String],
-                   recordType: Seq[String], male: Double, female: Double)
+case class Result3(countMale: Double, countFemale: Double, countBoth: Double)
 
 object Job extends App {
 
@@ -32,20 +32,7 @@ object Job extends App {
   val population = sc.textFile(Parameters.path_population)
     .map(CityPopulation(_))
 
-  // поиск достоверных данных
-  // от менее достоверных. К самым достоверным
-  // Other estimate
-  // Provisional figure
-  // Final figure, complete
-
-  // Определение населения от менее предпочитаемого к самому предпочитаеому
-  // Estimate - de jure  (граждане)
-  // Estimate - de facto  (присутствующее) в день переписи
-  // Sample survey - de facto
-  // Census - de jure - complete tabulation
-  // Census - de facto - complete tabulation
-
-  population.map(pp => ((pp.countryOrArea, pp.year, pp.city, pp.cityType, pp.recordType, pp.reliability, pp.sourceYear),
+  val temp = population.map(pp => ((pp.countryOrArea, pp.year, pp.city, pp.cityType, pp.recordType, pp.reliability, pp.sourceYear),
     (pp.sex, pp.value)))
     .aggregateByKey(Result(Set.empty[String], Seq.empty[Double], 0))((acc, pp) => {
       val sex = acc.uniqSex + pp._1
@@ -61,20 +48,56 @@ object Job extends App {
           two.value ++ one.value,
           one.sumPopulation + two.sumPopulation)
     }).map { case (key, result) => ((key._1, key._2, key._3, key._4),
-      // head - male    last - female  sumPopulation - both
+    // head - male    last - female  sumPopulation - both
     (key._5, key._6, result.value.head, result.value.last, result.sumPopulation))
   }
-  // На данном этапе возможна такая ситауция. Нужно оставить самое точное значение(точность зависит от наших предпочтений)
-  // ("Austria",2011,"Bregenz","City proper")	("Census - de jure - complete tabulation","Final figure, complete",13334.0,14497.0,27831.0)
-  //("Austria",2011,"Bregenz","City proper")	("Estimate - de jure","Final figure, complete",13360.0,14424.0,27784.0)
-  // ("Austria",2011,"Bregenz","City proper")	("Census - de facto - complete tabulation","Final figure, complete",13234.0,14497.0,27831.0)
-  //("Austria",2011,"Bregenz","City proper")	("Estimate - de facto","Final figure, complete",13960.0,14424.0,27784.0)
+    // На данном этапе возможна такая ситауция. Нужно оставить самое точное значение(точность зависит от наших предпочтений)
+    // ("Austria",2011,"Bregenz","City proper")	("Census - de jure - complete tabulation","Final figure, complete",13334.0,14497.0,27831.0)
+    //("Austria",2011,"Bregenz","City proper")	("Estimate - de jure","Final figure, complete",13360.0,14424.0,27784.0)
+    // ("Austria",2011,"Bregenz","City proper")	("Census - de facto - complete tabulation","Final figure, complete",13234.0,14497.0,27831.0)
+    //("Austria",2011,"Bregenz","City proper")	("Estimate - de facto","Final figure, complete",13960.0,14424.0,27784.0)
     .groupByKey()
-    .map{ case (key, stat) => (key, stat.maxBy( el =>helpFunc.help.relevance(el._1, el._2)))}
+    .map { case (key, stat) => (key, stat.maxBy(el => helpFunc.help.relevance(el._1, el._2))) }
   // на данном этапе найдено наиболее актуальное число жителей в конкретном городе в конкретный год
-    .map {
-      case (key, result) =>
-        s"$key\t$result"
-    }.saveAsTextFile(Parameters.output_path)
+  // теперь можно переходить к решению задачи
 
+  // ((countryOrArea, year, ),    (city,  cityType, male,    female,  bouth))
+  val statisticByCity = temp.map(j => ((j._1._1, j._1._2), (j._1._3, j._1._4, j._2._3, j._2._4, j._2._5)))
+    .filter(g => g._2._2 == "\"City proper\"")
+    .aggregateByKey(Result2(0, Seq.empty[(String, Double)]))((acc, pp) => {
+      if (pp._5 > 1000000)
+        Result2(acc.countCity + 1, acc.city :+ (pp._1, pp._5))
+      else
+        Result2(acc.countCity, acc.city :+ (pp._1, pp._5))
+    }, (one, _) => {
+      Result2(one.countCity, one.city)
+    }).map { case (key, result) => ((key._1, key._2),
+    (result.countCity, result.city.sortBy(f => f._2).reverse
+      .take(5).map(el => el._1)))
+  } // посчитано кол-во городов миллиоников и топ 5 городов
+
+
+  val populationCountry = temp.map(j => ((j._1._1, j._1._2, j._1._3), (j._1._4, j._2._3, j._2._4, j._2._5)))
+    .groupByKey()
+    .map { case (key, stat) => (key, stat.maxBy(el => helpFunc.help.typeCity(el._1))) } // выбирал самые подходящие записи для подсчета общей численности населения стран
+    .map(el => ((el._1._1, el._1._2), (el._2._2, el._2._3)))
+    //.map{ case ((countryOrArea,year,_),(_, male, female, _)) => ((countryOrArea,year),(male, female))}
+    .aggregateByKey(Result3(0, 0, 0))((acc, pp) => {
+    val male = pp._1
+    val female = pp._2
+    Result3(acc.countMale + male, acc.countFemale + female, acc.countBoth + male + female)
+  }, (one, _) => {
+    Result3(one.countMale, one.countFemale, one.countBoth)
+    // (countryOrArea,year,countMale,countFemale,countBoth)
+  }).map(el=>(el._1._1, el._1._2, el._2.countMale/el._2.countFemale, el._2.countBoth))
+    // подсчитано соотношение мужского и женского населения, а также общее население стран
+
+   //для души подсчет населения мира :)
+    populationCountry.map(el=>(el._2,el._4))
+      .groupByKey()
+      .map(el=>(el._1, el._2.sum))
+      .map {
+        case (key, result) =>
+          s"$key\t$result"
+      }.saveAsTextFile(Parameters.output_path)
 }
--- src/main/scala/solution/helpFunc/help.scala
+++ src/main/scala/solution/helpFunc/help.scala
@@ -2,6 +2,19 @@ package solution.helpFunc
 
 object help {
 
+  // поиск достоверных данных
+  // от менее достоверных. К самым достоверным
+  // Other estimate
+  // Provisional figure
+  // Final figure, complete
+
+  // Определение населения от менее предпочитаемого к самому предпочитаеому
+  // Estimate - de jure  (граждане)
+  // Estimate - de facto  (присутствующее) в день переписи
+  // Sample survey - de facto
+  // Census - de jure - complete tabulation
+  // Census - de facto - complete tabulation
+
   def matchRecordType(recordType: String): Int = recordType match {
     case "\"Census - de facto - complete tabulation\"" => 5
     case "\"Census - de jure - complete tabulation\"" => 4
@@ -20,5 +33,12 @@ object help {
 
   def relevance(recordType: String, reliability: String): Int ={
     matchReliability(reliability) + matchRecordType(recordType)
+  }
+
+
+  def typeCity(typeC: String): Int = typeC match {
+    case "\"Urban agglomeration\"" => 3
+    case "\"City proper\"" => 2
+    case _ => 1
   }
 }
